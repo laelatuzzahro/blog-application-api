af9eb11f7c27a568a5fdafbcf11a11de002bc622
laelatuzzahro <laelatuzzahro88@gmail.com>
2023-05-25 23:35:40 +0700

    first commit

---
+++ src/test/java/challenge/blogapplicationapi/BlogApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.blogapplicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
